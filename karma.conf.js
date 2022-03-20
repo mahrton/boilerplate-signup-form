8b82a235008b5a85f6f900e4e876d2da43e838df
Marton Tatai <marton.tatai@kpn.com>
2022-03-20 11:26:33 +0100

    - feature: rename project to be kore accurate

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fedex-login'),
+      dir: require('path').join(__dirname, './coverage/fedex-signup'),
       subdir: '.',
       reporters: [
         { type: 'html' },
