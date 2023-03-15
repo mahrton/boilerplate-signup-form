3835088afb371c71eb6327caf582043e5f7c7501
Marton Tatai <cethal@gmail.com>
2023-03-15 22:13:48 +0100

    feature: upgrade material to 15

--- src/app/components/fields/password-field/password-field.component.spec.ts
+++ src/app/components/fields/password-field/password-field.component.spec.ts
@@ -2,8 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PasswordFieldComponent } from './password-field.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
+import {MatLegacyFormFieldModule as MatFormFieldModule} from "@angular/material/legacy-form-field";
+import {MatLegacyInputModule as MatInputModule} from "@angular/material/legacy-input";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 describe('PasswordFieldComponent', () => {
